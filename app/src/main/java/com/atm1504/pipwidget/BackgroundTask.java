053fc1b79d3d014e6f17c4c4987bd241b177516a
Amartya Mondal <atm1504.in@gmail.com>
2021-08-10 00:08:31 +0530

    Add runner

---
+++ app/src/main/java/com/atm1504/pipwidget/BackgroundTask.java
@@ -0,0 +1,59 @@
+package com.atm1504.pipwidget;
+
+import android.content.Context;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class BackgroundTask extends Worker {
+    public BackgroundTask(
+            @NonNull Context appContext,
+            @NonNull WorkerParameters workerParams) {
+        super(appContext, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        runLongLoop();
+        return Result.success();
+    }
+
+    public void runLongLoop() {
+        Thread thread = new Thread() {
+            public void run() {
+                Looper.prepare();
+                Handler mHandler = new Handler();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        RunTimer();
+                        runLongLoop();
+                    }
+                }, 10000);
+                Looper.loop();
+            }
+        };
+        thread.start();
+    }
+
+    public void RunTimer() {
+        new CountDownTimer(9000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Log.d("ATM", "seconds remaining:  " + millisUntilFinished);
+            }
+
+            public void onFinish() {
+                Log.d("ATM", "Finished the timer");
+            }
+
+        }.start();
+    }
+
+}
--- app/src/main/java/com/atm1504/pipwidget/MainActivity.java
+++ app/src/main/java/com/atm1504/pipwidget/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.PictureInPictureParams;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Rational;
 import android.view.Display;
 import android.view.View;
@@ -12,11 +13,15 @@ import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button enter;
     ActionBar actionBar;
+    private WorkManager mWorkManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,27 +30,41 @@ public class MainActivity extends AppCompatActivity {
 
         actionBar = getActionBar();
         enter = findViewById(R.id.enter_button);
-
+        mWorkManager = WorkManager.getInstance(getApplication());
+        Log.d("ATM", "I am here");
         enter.setOnClickListener(new View.OnClickListener() {
+
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                Display d = getWindowManager()
-                        .getDefaultDisplay();
-                Point p = new Point();
-                d.getSize(p);
-                int width = p.x;
-                int height = p.y;
-
-                Rational ratio
-                        = new Rational(width, height);
-                PictureInPictureParams.Builder
-                        pip_Builder
-                        = new PictureInPictureParams
-                        .Builder();
-                pip_Builder.setAspectRatio(ratio).build();
-                enterPictureInPictureMode(pip_Builder.build());
+                triggerPiP();
             }
         });
+
+        triggerRunner();
+    }
+
+    public void triggerRunner() {
+        Log.d("ATM", "Insider triggering of runner");
+        mWorkManager.enqueue(OneTimeWorkRequest.from(BackgroundTask.class));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void triggerPiP() {
+        Display d = getWindowManager()
+                .getDefaultDisplay();
+        Point p = new Point();
+        d.getSize(p);
+        int width = p.x;
+        int height = p.y;
+
+        Rational ratio
+                = new Rational(width, height);
+        PictureInPictureParams.Builder
+                pip_Builder
+                = new PictureInPictureParams
+                .Builder();
+        pip_Builder.setAspectRatio(ratio).build();
+        enterPictureInPictureMode(pip_Builder.build());
     }
 }
